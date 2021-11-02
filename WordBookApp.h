a12b04415a8efa7b39ea8fa5346fa64e24bc4a6e
인재 박 <injae7034@gmail.com>
2021-11-02 22:04:58 +0900

    초기 커밋.

---
+++ WordBookApp.h
@@ -0,0 +1,11 @@
+#ifndef _WORDBOOKAPP_H
+#define _WORDBOOKAPP_H
+#include<afxwin.h>//CWnd 헤더파일
+
+class WordBookApp :public CWinApp
+{
+public:
+	virtual BOOL InitInstance();
+};
+#endif // !_WORDBOOKAPP_H
+
